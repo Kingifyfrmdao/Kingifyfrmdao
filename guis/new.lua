ae692436121936474b8cc622e29d7d11f32dc3e1
Kingifyfrmdao <jeremiahoue@gmail.com>
2026-06-03 18:12:47 +0100

    Update GUI error messages and links

--- guis/new.lua
+++ guis/new.lua
@@ -136,7 +136,7 @@ local function safecall(func, ...)
 	xpcall(function()
 		func(unpack(args))
 	end, function(err)
-		warn("[SKIDV7] GUI Error: "..err)
+		warn("[KingV4] GUI Error: "..err)
 	end)
 end
 
@@ -4773,7 +4773,7 @@ function mainapi:CreateGUI()
 
 		task.spawn(function()
 			tooltip.Text = 'Copied!'
-			setclipboard('https://discord.gg/5gJqhQmrdS')
+			setclipboard('https://discord.gg/fffNfUwgCn')
 		end)
 	end)
 	settingsbutton.MouseEnter:Connect(function()
@@ -7205,7 +7205,7 @@ local scarcitybanner = Instance.new('TextLabel')
 scarcitybanner.Size = UDim2.fromScale(1, 0.02)
 scarcitybanner.Position = UDim2.fromScale(0, 0.97)
 scarcitybanner.BackgroundTransparency = 1
-scarcitybanner.Text = 'Join SkidV7 server to report any bugs, suggestions or buy prem, click discord icon :D'
+scarcitybanner.Text = 'Join KingV4 server to report any bugs, suggestions or buy prem, click discord icon :D'
 scarcitybanner.TextScaled = true
 scarcitybanner.TextColor3 = Color3.new(1, 1, 1)
 scarcitybanner.TextStrokeTransparency = 0.5
